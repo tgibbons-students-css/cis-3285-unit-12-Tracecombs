fc99cc9dfcf3099eb9578abf76278433cfc4feff
tracecombs <tcombs1@css.edu>
2016-11-22 10:48:04 -0600

    CHanges for class monday 11/22

--- SubtypeCovariance/UserRepository.cs
+++ SubtypeCovariance/UserRepository.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubtypeCovariance
-{
-    public class UserRepository : EntityRepository
+namespace SubtypeCovariance { 
+
+    public class UserRepository : IEntityRepository<User>
     {
         //public override Entity GetByID(Guid id)
-        public override User GetByID(Guid id)
+        public User GetByID(Guid id)
         {
             return new User(id);
         }
