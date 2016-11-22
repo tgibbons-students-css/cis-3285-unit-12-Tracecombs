ba3fa7b5defe48b81991d941cab5d958a9605720
tracecombs <tcombs1@css.edu>
2016-11-22 11:04:05 -0600

    Commit #2

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,13 +13,20 @@ namespace ConsoleSample
         static void Main(string[] args)
         {
 
-            EntityRepository entityRepository = new EntityRepository();
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
 
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetByID(Guid.NewGuid());
+
+            myUser.Name = "Tom";
+            Console.WriteLine("New user ID: " + myUser.ID.ToString());
+            Console.WriteLine("New user Email: " + myUser.EmailAddress.ToString());
+            Console.WriteLine("New user DOB: " + myUser.DateOfBirth);
         }
     }
 }
